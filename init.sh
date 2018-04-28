699de8cdd23a81c2f3ca595fe55f0fe4173471f9
Leo Ackerman <lackerman@users.noreply.github.com>
2018-04-28 21:35:42 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-
-ln -s $PWD/.vim $HOME/.vim
-ln -s $PWD/.vimrc $HOME/.vimrc
-ln -s $PWD/.zshrc $HOME/.zshrc
-ln -s $PWD/.tmux.conf $HOME/.tmux.conf
-
-zsh
+#!/bin/zsh
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
@@ -13,5 +6,12 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 	ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+rm -f $HOME/.zshrc
+ln -s $PWD/.zshrc $HOME/.zshrc
+
+ln -s $PWD/.vim $HOME/.vim
+ln -s $PWD/.vimrc $HOME/.vimrc
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 
 chsh -s /bin/zsh
