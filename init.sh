90bba4d4faf51ecad6d7bb8eeb51636d45038069
Leo Ackerman <ackerman.leo@gmail.com>
2018-08-08 21:37:52 +0200

    Added support for linux

--- init.sh
+++ init.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-wget https://github.com/justjanne/powerline-go/releases/download/v1.11.0/powerline-go-darwin-amd64 -O $HOME/.dotfiles/bin/powerline-go
-chmod +x $HOME/.dotfiles/bin/powerline-go
+dotfiles=$HOME/.dotfiles
+mkdir -p $dotfiles/bin
 
 if [[ -f $HOME/.profile ]]; then
 	rm $HOME/.profile
 fi
 
-ln -s $PWD/.profile $HOME/.profile
-ln -s $PWD/.vim $HOME/.vim
-ln -s $PWD/.vimrc $HOME/.vimrc
-ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+ln -s $dotfiles/.vim $HOME/.vim
+ln -s $dotfiles/.vimrc $HOME/.vimrc
+ln -s $dotfiles/.tmux.conf $HOME/.tmux.conf
+
+# Setup the profile file according to the system
+if [[ "$OSTYPE" == "linux"* ]]; then
+	wget -O $dotfiles/bin/powerline-go https://github.com/justjanne/powerline-go/releases/download/v1.11.0/powerline-go-linux-amd64
+	ln -s $dotfiles/.profile $HOME/.bashrc
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	wget -O $dotfiles/bin/powerline-go https://github.com/justjanne/powerline-go/releases/download/v1.11.0/powerline-go-darwin-amd64
+	ln -s $dotfiles/.profile $HOME/.profile
+fi
+chmod +x $dotfiles/bin/powerline-go
