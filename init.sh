ac847275fa06128b908772610a1085bd88039ce0
Leo Ackerman <ackerman.leo@gmail.com>
2019-06-03 22:20:38 +0200

    Uppercased the DOTFILES variable name

--- init.sh
+++ init.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-dotfiles=$HOME/.dotfiles
+DOTFILES=$HOME/.dotfiles
 if [[ -f $HOME/.profile ]]; then
 	rm $HOME/.profile
 fi
 
-ln -s $dotfiles/.vim $HOME/.vim
-ln -s $dotfiles/.vimrc $HOME/.vimrc
-ln -s $dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s ${DOTFILES}/.vim $HOME/.vim
+ln -s ${DOTFILES}/.vimrc $HOME/.vimrc
+ln -s ${DOTFILES}/.tmux.conf $HOME/.tmux.conf
 
 # Setup the profile file according to the system
 if [[ "$OSTYPE" == "linux"* ]]; then
-	ln -s $dotfiles/.profile $HOME/.bashrc
+	ln -s ${DOTFILES}/.profile $HOME/.bashrc
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	ln -s $dotfiles/.profile $HOME/.profile
+	ln -s ${DOTFILES}/.profile $HOME/.profile
 	brew install bash-completion
 	brew install kubectx
 	brew install kubernetes-cli
